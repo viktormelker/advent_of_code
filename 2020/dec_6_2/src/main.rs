782866125042e09e157a6adad1ac6f279f4a048c
Viktor Melker <viktormelker@gmail.com>
2020-12-09 18:27:03 +0100

    fix dec 6_2

--- 2020/dec_6_2/src/main.rs
+++ 2020/dec_6_2/src/main.rs
@@ -12,7 +12,6 @@ impl Declaration {
 
         for input in group_input.iter() {
             let current_chars: HashSet<char> = input.chars().collect();
-            // println!("Found chars {:?}", current_chars);
             all_group_chars = all_group_chars.intersection(&current_chars).copied().collect();
         }
 
@@ -27,13 +26,8 @@ impl Declaration {
 }
 
 fn main() {
-    let declaration_input = read_file("./data/test_input.txt");
+    let declaration_input = read_file("./data/input.txt");
     let declarations = parse_declarations(&declaration_input);
-
-    for declaration in declarations.iter() {
-        println!("Found declaration for group {:?}", declaration);
-    }
-
     let total_answers: u64 = declarations.iter().fold(0, |acc, declaration| acc + declaration.size());
     println!("Found a total of {} answers", total_answers);
 }
@@ -46,7 +40,11 @@ fn read_file(filename: &str) -> String {
 
 fn parse_declarations(declaration_input: &str) -> Vec<Declaration> {
     let mut declarations: Vec<Declaration> = vec![];
-    let declaration_groups: Vec<&str> = declaration_input.split("\n\n").collect();
+    let declaration_groups: Vec<&str> = declaration_input
+        .split("\n\n")
+        .map(|declaration_str| declaration_str.trim())
+        .collect();
+    println!("Found {} declarations", declaration_groups.len());
 
     for declaration_group in declaration_groups.iter() {
         declarations.push(Declaration::from_group_input(declaration_group.split("\n").collect()));
