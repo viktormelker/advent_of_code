4695c57ea8de6af4cdf3a3e91651ce8c9b637a7c
Viktor Melker <viktormelker@gmail.com>
2021-12-05 21:31:05 +0100

    Implement first star reqs

--- 2021/dec_3/src/report.rs
+++ 2021/dec_3/src/report.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::str::FromStr;
+use std::str::Lines;
 
 #[derive(Debug, Clone)]
 pub struct ParseDiagnosticDataError;
@@ -29,9 +30,75 @@ impl FromStr for DiagnosticData {
     type Err = ParseDiagnosticDataError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        // TODO: Implement
+        let char_to_count = '1';
+        let alternative_char = '0';
+        let character_count = count_char(char_to_count, s.lines());
+        let gamma_string = character_count
+            .count
+            .iter()
+            .map(|count| {
+                get_most_common_bit(
+                    char_to_count,
+                    *count,
+                    alternative_char,
+                    character_count.length,
+                )
+            })
+            .collect::<String>();
 
-        let result = DiagnosticData{gamma_rate: 0, epsilon_rate: 0};
+        let epsilon_string = character_count
+            .count
+            .iter()
+            .map(|count| {
+                get_least_common_bit(
+                    char_to_count,
+                    *count,
+                    alternative_char,
+                    character_count.length,
+                )
+            })
+            .collect::<String>();
+
+        let result = DiagnosticData {
+            gamma_rate: u32::from_str_radix(&gamma_string, 2).unwrap(),
+            epsilon_rate: u32::from_str_radix(&epsilon_string, 2).unwrap(),
+        };
         Ok(result)
     }
+}
+
+struct CountCharResult {
+    count: Vec<u32>,
+    length: u32,
+}
+
+fn count_char(character: char, iter: Lines<'_>) -> CountCharResult {
+    let data_length = 12;
+    let mut result = CountCharResult {
+        count: vec![0; data_length],
+        length: 1000,
+    };
+
+    for text_line in iter {
+        for (index, c) in text_line.chars().enumerate() {
+            if c == character {
+                result.count[index] = result.count[index] + 1;
+            }
+        }
+    }
+    result
+}
+
+fn get_most_common_bit(character: char, count: u32, alternative_char: char, length: u32) -> char {
+    if count > length / 2 {
+        return character;
+    } else if count < length / 2 {
+        return alternative_char;
+    } else {
+        panic!("Impossible to decide most common char!");
+    }
+}
+
+fn get_least_common_bit(character: char, count: u32, alternative_char: char, length: u32) -> char {
+    get_most_common_bit(alternative_char, count, character, length)
 }
